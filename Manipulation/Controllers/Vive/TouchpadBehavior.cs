577d40e135e7f3d9bf70891ecc2f24d9379dbc4d
Thomas Laptop <junichifujio@gmail.com>
2017-09-25 13:11:49 -0700

    Restructured ASL for future work. Created several folders in ASL structure to separate logic for different aspects of ASL: Avatars, Manipulation (Controllers & Tracking), Scenes, and UI (HUD & Menus). Generated prefabs folder in Resources folder for future prefab allocation (outside of submodule work). This resources folder should be differentiated from the general Resources folder so to provide core resources necessary for all project nodes to run. The general Resources submodule/folder should be used for extraneous resources (e.g. Trophies, cars, etc.).
    Integrated ViveDemo core logic, scenes, and prefabs into ASL. Changed names of some ViveDemo items and altered namespace to reflect new folder structure.
    
    Must rename UWBNetworkingPackage folder and namespace to ASL.Networking.
    Must refactor some Vive scripts to separate the logic (ControllerUIManager into CustomRadialMenu).
    Must move/refactor all scenes outside of submodules into the Scenes folder for easier access.

---
+++ Manipulation/Controllers/Vive/TouchpadBehavior.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using VRTK;
+
+using ASL.Avatars.Vive;
+
+namespace ASL.Manipulation.Controllers.Vive
+{
+    /// <summary>
+    /// Script to be added to the Camera Rig's Right and Left controllers. Adds object instantiation
+    /// functionality on touchpad press. 
+    /// </summary>
+    public class TouchpadBehavior : VRTK_ControllerEvents
+    {
+        public string PrefabString = "Trophy";
+        private string ViveAvatarName = "ViveAvatar";
+
+        /// <summary>
+        /// Subscribe TouchPad events to be called appropriately.
+        /// </summary>
+        void Start()
+        {
+            TouchpadPressed += OnTouchpadPressedHandler;
+        }
+
+        /// <summary>
+        /// When the touchpad is pressed, instantiate a cube, located at the origin of the game
+        /// space
+        /// </summary>
+        /// <param name="obj">Reference to the controller that pressed the touchpad</param>
+        /// <param name="e">Controller event arguments</param>
+        public void OnTouchpadPressedHandler(object obj, ControllerInteractionEventArgs e)
+        {
+            // Generate a prefab at the origin
+            //PhotonNetwork.Instantiate(PrefabString, Vector3.zero, Quaternion.identity, 0);
+
+            // Generate a prefab two feet in front of your face
+            Vector3 HeadPos = GameObject.Find(ViveAvatarName).GetComponent<ViveHead>().HeadPosition;
+            Vector3 PrefabPos = HeadPos + new Vector3(0, 1, 0);
+            PhotonNetwork.Instantiate(PrefabString, PrefabPos, Quaternion.identity, 0);
+        }
+    }
+}
